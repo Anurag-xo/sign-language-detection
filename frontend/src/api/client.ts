53bf5aca00c532401fe11f55e291891e3db89984
Anurag-xo <amnrag889@gmail.com>
2025-11-21 09:17:08 +0000

    fix: typo

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,4 +1,7 @@
+import { QueryClient } from "@tanstack/react-query";
 import axios from "axios";
+
+export const queryClient = new QueryClient();
 
 const apiClient = axios.create({
   baseURL: "http://127.0.0.1:5000", // URL of the Python backend
